095337f488b625af4d2528ccc003a1dd10dc21a9
cherrydub <cherrydub@gmail.com>
2024-10-20 17:01:46 +0100

    refactor: tidy up

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,15 +1,21 @@
-import DeployButton from "@/components/DeployButton";
-import Link from "next/link";
-import { Button } from "../ui/button";
+import Link from 'next/link'
 
-export default function Header() {
+import DeployButton from '@/components/DeployButton'
+
+import { Button } from '../ui/button'
+
+export default function Header () {
   return (
-    <nav className="w-full flex-center gap-2 border-b border-b-foreground/10 h-16">
-          
-
-          <Link className="font-semibold text-sm" href={"/"}><Button size={'sm'}>🍒 cherrydub</Button></Link>
-            <DeployButton />
-
+    <nav className='w-full flex-center gap-2 border-b border-b-foreground/10 h-16'>
+      <Link className='font-semibold text-sm' href={'/'}>
+        <Button
+          className='text-xs grayscale hover:grayscale-0 space-x-2'
+          size={'sm'}
+        >
+          cherrydub 🌐
+        </Button>
+      </Link>
+      <DeployButton />
     </nav>
-  );
+  )
 }
